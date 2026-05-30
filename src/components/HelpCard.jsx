d1909cd55176e796c5569ceb02fd67fda7a17089
arishiiiik <arishiiiik@example.com>
2026-05-30 18:12:18 +0300

    Add missing export default to HelpCard

--- src/components/HelpCard.jsx
+++ src/components/HelpCard.jsx
@@ -1,16 +1,17 @@
+import { useModal } from '../context/ModalContext'
 import { MEDIA_URL } from '../services/api'
 
 function HelpCard({ icon, title, description, buttonText, modalType }) {
-  const imageUrl = icon ? `${MEDIA_URL}${icon}` : null
   const { openDonationModal, openPartnerModal, openVolunteerModal } = useModal()
-  
+  const imageUrl = icon ? `${MEDIA_URL}${icon}` : null
+
   const handleClick = () => {
     if (modalType === 'donation') openDonationModal()
     else if (modalType === 'partner') openPartnerModal()
     else if (modalType === 'volunteer') openVolunteerModal()
     else alert('Форма в разработке')
   }
-  
+
   return (
     <div className="help_block">
       {imageUrl && <img className="help_img" src={imageUrl} alt={title} />}
@@ -22,3 +23,5 @@ function HelpCard({ icon, title, description, buttonText, modalType }) {
     </div>
   )
 }
+
+export default HelpCard   // ← ЭТА СТРОЧКА ОБЯЗАТЕЛЬНА
